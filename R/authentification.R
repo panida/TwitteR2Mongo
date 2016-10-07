4c6f6b9b372dc8a0e8213962a5f6becff4126d99
panida <cp.panida@gmail.com>
2016-10-07 11:05:40 +0700

    direct oauth

--- R/authentification.R
+++ R/authentification.R
@@ -15,13 +15,15 @@
 #'
 #' @export
 
-setAPICredentials <- function(key, secret) {
+setAPICredentials <- function(key, secret,access_token="",access_secret="") {
   if ((!class(key) == "character") ||
       (!class(secret) == "character")) {
     stop("Key and sectet must be character")
   }
   Sys.setenv(twitter_key = key)
   Sys.setenv(twitter_secret = secret)
+  Sys.setenv(access_token = access_token)
+  Sys.setenv(access_secret = access_secret)
   message("Credentials stored, make sure to set callback url to http://127.0.0.1:1410")
 }
 
@@ -40,13 +42,38 @@ setAPICredentials <- function(key, secret) {
 #' @export
 #'
 
+get_twitter_token_via_sign = function(app, access_token, access_secret) {
+  print("Using direct authentication")
+  params <- list(as_header = TRUE)
+  credentials <- list(oauth_token = access_token, 
+                      oauth_token_secret = access_secret)
+  twitter_token <- httr::Token1.0$new(endpoint = NULL, params = params, 
+                                app = app, credentials = credentials)
+
+  if (is.null(twitter_token))
+    stop("Invalid response for twitter_token")
+
+  twitter_token
+}
+
+get_twitter_token_via_browser = function(app, ...) {
+  print("Using browser based authentication") 
+  httr::oauth1.0_token(httr::oauth_endpoints('twitter'), app)
+}
+  
 makeOAuth <-
-  function(key = Sys.getenv("twitter_key"), secret = Sys.getenv("twitter_secret")) {
+  function(key = Sys.getenv("twitter_key"), secret = Sys.getenv("twitter_secret"),access_token=Sys.getenv("access_token"), access_secret=Sys.getenv("access_secret")) {
     # Generate or load authetification token, please store the credentials
 
     if ((key == "") || (secret == "")) {
       stop("Twitter key or secret are not set properly, use setAPICredentials to fix this.")
     }
+    if ((key == "") || (secret == "")) {
+      token_func = get_twitter_token_via_browser
+    } else {
+      token_func = get_twitter_token_via_sign
+    }
     myapp <- httr::oauth_app("twitter", key = key, secret = secret)
-    return (httr::oauth1.0_token(httr::oauth_endpoints("twitter"), myapp))
+    twitter_token = token_func(app, access_token, access_secret)
+    return (twitter_token)
   }
